9a279b41b17575085f4422a892ee844b8ac00f0e
Giaeulate <giaeulate@gmail.com>
2023-03-24 11:14:33 -0400

    secret key modified

--- web/settings.py
+++ web/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8*s3fd+3(a**=bhenuhl)&b_gaa)e_3zxpo4t@e8%7v6nqbuh5"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
